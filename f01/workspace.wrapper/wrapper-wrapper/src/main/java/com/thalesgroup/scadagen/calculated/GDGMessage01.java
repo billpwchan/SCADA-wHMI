a5af6bc8809f357f7a2302e2d951a42e9827c06b
syau <steven.yau@thalesgroup.com>
2016-07-29 17:26:30 +0800

    SCADAGEN-272
    Add Generic Static Computer for GDGMessage01 to GDGMessage10

---
+++ f01/workspace.wrapper/wrapper-wrapper/src/main/java/com/thalesgroup/scadagen/calculated/GDGMessage01.java
@@ -0,0 +1,26 @@
+package com.thalesgroup.scadagen.calculated;
+
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GDGMessage01 extends GDGMessage {
+	private final Logger s_logger		= LoggerFactory.getLogger(GDGMessage01.class.getName());
+
+	@Override
+	public String getComputerId() {
+		return this.getClass().getName();
+	}
+	
+	public GDGMessage01 () {
+		super();
+    	
+    	String classnames [] = getComputerId().split(Pattern.quote("."));
+    	classname = classnames[classnames.length-1];
+    	
+    	s_logger.info("{} getComputerId()[{}]", classname, getComputerId());
+    	s_logger.info("{} classname[{}]", classname, classname);
+    	
+    }
+}

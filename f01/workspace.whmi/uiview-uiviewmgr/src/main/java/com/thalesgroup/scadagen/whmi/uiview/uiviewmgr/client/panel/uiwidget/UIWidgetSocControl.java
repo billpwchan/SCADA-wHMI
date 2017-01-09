7ea87260545cdebd9c72288bc4c588d2e2eba457
syau <syau>
2017-01-09 12:20:02 +0800

    SCADAGEN-593
    Update Widget

--- f01/workspace.whmi/uiview-uiviewmgr/src/main/java/com/thalesgroup/scadagen/whmi/uiview/uiviewmgr/client/panel/uiwidget/UIWidgetSocControl.java
+++ f01/workspace.whmi/uiview-uiviewmgr/src/main/java/com/thalesgroup/scadagen/whmi/uiview/uiviewmgr/client/panel/uiwidget/UIWidgetSocControl.java
@@ -1,9 +1,6 @@
 package com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget;
 
 import java.util.HashMap;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.ui.Widget;
@@ -17,9 +14,11 @@ import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIEven
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIEventActionProcessorMgr;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIEventActionProcessor_i;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIActionEventAttribute_i.ActionAttribute;
+import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIActionEventAttribute_i.UIActionEventTargetAttribute;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIActionEventAttribute_i.UIActionEventType;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.common.UIView_i.ViewAttribute;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget.UIWidgetDataGrid_i.DataGridEvent;
+import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget.UIWidgetSocAutoManuControl_i.AutoManuEvent;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget.UIWidgetSocControl_i.ParameterName;
 import com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget.soc.Equipment_i;
 import com.thalesgroup.scadagen.whmi.uiwidget.uiwidget.client.UIWidget_i;
@@ -42,11 +41,22 @@ public class UIWidgetSocControl extends UIWidget_i {
 	
 	private UIEventActionProcessor_i uiEventActionProcessor_i = null;
 	
-	private String targetDataGridColumn = "";
+	private final String strPrepareGrc = "PrepareGrc";
+	private final String strLaunchGrc = "LaunchGrc";
+	private final String strAbortGrc = "AbortGrc";
+	
 	private String targetDataGrid		= "";
+	private String targetDataGridColumn = "";
+	private String targetDataGridColumn2 = "";
 	
 	private String datagridSelected = null;
 	private Equipment_i equipmentSelected = null;
+	
+	private int selectedStep = -1;
+	
+	private int autoMenu = -1;
+	
+	private int [] skips = null;
 
 	private UIWidgetCtrl_i uiWidgetCtrl_i = new UIWidgetCtrl_i() {
 		
@@ -71,25 +81,26 @@ public class UIWidgetSocControl extends UIWidget_i {
 						String actionsetkey = element;
 						
 						// build scsEnvId
-						String scsenvid = "OCCENV";
+						String scsenvid = equipmentSelected.getStringValue(targetDataGridColumn);
+						
+						logger.info(className, function, "scsenvid[{}]", scsenvid);
 						
 						// build dbalias						
-						String alias = equipmentSelected.getStringValue(targetDataGridColumn);
-												
-						String dbalias = alias;
+						String dbalias = equipmentSelected.getStringValue(targetDataGridColumn2);
 						
 						logger.info(className, function, "dbalias[{}]", dbalias);
 						
-						String strPrepareGrc = "PrepareGrc";
-						String strLaunchGrc = "LaunchGrc";
-						String strAbortGrc = "AbortGrc";
+						int iExecuteMode = getAutoManuMode();
+						String executemode = String.valueOf(iExecuteMode);
+//						String executemode = String.valueOf(GrcExecMode.StopOnFailed.getValue());
 						
-						short executemode = (short)min(max(GrcExecMode.StopOnFailed.getValue(), Short.MIN_VALUE), Short.MAX_VALUE);
+						int iCurretnStep = getCurStep();
+						String curstep = String.valueOf(iCurretnStep);
+//						String curstep = String.valueOf(0);
 						
-						int curstep = 0;
-						
-						int skipstep [] = new int[]{};
-						
+						int [] iSkips = getSkips();
+						String skips = convertToStringSkips(iSkips);
+//						String skipstep = "";
 						// build key
 						String strKeyPrepareGrc	= "grc"+strPrepareGrc+"_"+className+"_"+scsenvid+"_"+dbalias;
 						String strKeyLaunchGrc	= "grc"+strLaunchGrc+"_"+className+"_"+scsenvid+"_"+dbalias;
@@ -101,7 +112,7 @@ public class UIWidgetSocControl extends UIWidget_i {
 						
 						logger.info(className, function, "executemode[{}]", executemode);
 						logger.info(className, function, "curstep[{}]", curstep);
-						logger.info(className, function, "skipstep[{}]", skipstep);
+						logger.info(className, function, "skipstep[{}]", skips);
 						
 						HashMap<String, HashMap<String, Object>> override = new HashMap<String, HashMap<String, Object>>();
 						{
@@ -118,7 +129,7 @@ public class UIWidgetSocControl extends UIWidget_i {
 							parameter.put(ActionAttribute.OperationString4.toString(), dbalias);
 							parameter.put(ActionAttribute.OperationString5.toString(), executemode);
 							parameter.put(ActionAttribute.OperationString6.toString(), curstep);
-							parameter.put(ActionAttribute.OperationString7.toString(), skipstep);
+							parameter.put(ActionAttribute.OperationString7.toString(), skips);
 							override.put(strLaunchGrc, parameter);
 						}
 						{
@@ -147,7 +158,30 @@ public class UIWidgetSocControl extends UIWidget_i {
 			logger.info(className, function, "os1["+os1+"]");
 			
 			if ( null != os1 ) {
-				if ( os1.equals(DataGridEvent.RowSelected.toString() ) ) {
+				if ( os1.equals(AutoManuEvent.RadioBoxSelected.toString() ) ) {
+					
+					logger.info(className, function, "Store Selected RadioBox");
+					
+					String os2	= (String) uiEventAction.getParameter(ViewAttribute.OperationString2.toString());
+					
+					if ( null != os2 ) {
+						
+						logger.info(className, function, "os2[{}]", os2);
+						
+						String autoManu = os2;
+						
+						if ( autoManu.equals("auto") ) {
+							autoMenu = GrcExecMode.Auto.getValue();
+						} else if ( autoManu.equals("manu") ) {
+							autoMenu = GrcExecMode.StopOnFailed.getValue();
+						} else {
+							logger.warn(className, function, "os2[{}] type IS UNKNOW", os2);
+						}
+					} else {
+						logger.warn(className, function, "os2 IS NULL");
+					}
+					
+				} else if ( os1.equals(DataGridEvent.RowSelected.toString() ) ) {
 					
 					Object obj1 = uiEventAction.getParameter(ViewAttribute.OperationObject1.toString());
 					Object obj2 = uiEventAction.getParameter(ViewAttribute.OperationObject2.toString());
@@ -186,6 +220,18 @@ public class UIWidgetSocControl extends UIWidget_i {
 					} else {
 						logger.warn(className, function, "targetDataGrid IS NULL");
 					}
+				} else {
+					// General Case
+					String oe	= (String) uiEventAction.getParameter(UIActionEventTargetAttribute.OperationElement.toString());
+					
+					logger.info(className, function, "oe ["+oe+"]");
+					logger.info(className, function, "os1["+os1+"]");
+					
+					if ( null != oe ) {
+						if ( oe.equals(element) ) {
+							uiEventActionProcessor_i.executeActionSet(os1);
+						}
+					}
 				}
 			}
 			logger.end(className, function);
@@ -195,7 +241,6 @@ public class UIWidgetSocControl extends UIWidget_i {
 	@Override
 	public void init() {
 		final String function = "init";
-		
 		logger.begin(className, function);
 		
 		String strEventBusName = getStringParameter(ParameterName.SimpleEventBus.toString());
@@ -206,8 +251,9 @@ public class UIWidgetSocControl extends UIWidget_i {
 		String strHeader = "header";
 		DictionariesCache dictionariesCache = DictionariesCache.getInstance(strUIWidgetGeneric);
 		if ( null != dictionariesCache ) {
-			targetDataGridColumn	= dictionariesCache.getStringValue(optsXMLFile, ParameterName.TargetDataGridColumn.toString(), strHeader);
 			targetDataGrid			= dictionariesCache.getStringValue(optsXMLFile, ParameterName.TargetDataGrid.toString(), strHeader);
+			targetDataGridColumn	= dictionariesCache.getStringValue(optsXMLFile, ParameterName.TargetDataGridColumn.toString(), strHeader);
+			targetDataGridColumn2	= dictionariesCache.getStringValue(optsXMLFile, ParameterName.TargetDataGridColumn2.toString(), strHeader);
 		}
 		
 		logger.info(className, function, "targetDataGridColumn[{}]", targetDataGridColumn);
@@ -268,6 +314,53 @@ public class UIWidgetSocControl extends UIWidget_i {
 		uiEventActionProcessor_i.executeActionSetInit();
 		
 		logger.end(className, function);
+	}
+	
+	private int getAutoManuMode() {
+		final String function = "getCurStep";
+		logger.begin(className, function);
+		int result = -1;
+		result = autoMenu;
+		logger.end(className, function);
+		return result;		
+	}
+	
+	private int getCurStep() {
+		final String function = "getCurStep";
+		logger.begin(className, function);
+		int result = -1;
+		result = selectedStep;
+		logger.end(className, function);
+		return result;
+	}
+	
+	private String convertToStringSkips(int [] skips) {
+		final String function = "convertToStringSkips";
+		logger.begin(className, function);
+		String result = "";
+		if ( null != skips ) {
+			for ( int i = 0 ; i < skips.length ; ++i ) {
+				if ( result.length() > 0 ) result += ",";
+				result += String.valueOf(skips[i]);
+			}
+		} else {
+			logger.warn(className, function, "skips IS NULL");
+		}
+		logger.end(className, function);
+		return result;
+	}
+	
+	private int [] getSkips() {
+		final String function = "getSkips";
+		logger.begin(className, function);
+		int [] skips = null;
+		if ( null == this.skips ) {
+			skips = new int[]{};
+		} else {
+			skips = this.skips;
+		}
+		logger.end(className, function);
+		return skips;
 	}
 
 }
--- f01/workspace.whmi/uiview-uiviewmgr/src/main/java/com/thalesgroup/scadagen/whmi/uiview/uiviewmgr/client/panel/uiwidget/UIWidgetSocControl_i.java
+++ f01/workspace.whmi/uiview-uiviewmgr/src/main/java/com/thalesgroup/scadagen/whmi/uiview/uiviewmgr/client/panel/uiwidget/UIWidgetSocControl_i.java
@@ -1,26 +1,10 @@
 package com.thalesgroup.scadagen.whmi.uiview.uiviewmgr.client.panel.uiwidget;
 
-public interface UIWidgetSocControl_i {
-	// Event Send from View
-	public enum ViewerViewEvent {
-//		  FilterAdded("FilterAdded")
-//		, FilterRemoved("FilterRemoved")
-//		, RowSelected("RowSelected")
-//		, CounterValueChanged("CounterValueChanged")
-//		, AckVisible("AckVisible")
-//		, AckVisibleSelected("AckVisibleSelected")
-		;
-		private final String text;
-		private ViewerViewEvent(final String text) { this.text = text; }
-		@Override
-		public String toString() { return this.text; }
-	}
-	
+public interface UIWidgetSocControl_i {	
 	public enum ParameterName {
 		SimpleEventBus("SimpleEventBus")
 		, TargetDataGrid("TargetDataGrid")
 		, TargetDataGridColumn("TargetDataGridColumn")
-		, TargetDataGrid2("TargetDataGrid2")
 		, TargetDataGridColumn2("TargetDataGridColumn2")
 		;
 		private final String text;

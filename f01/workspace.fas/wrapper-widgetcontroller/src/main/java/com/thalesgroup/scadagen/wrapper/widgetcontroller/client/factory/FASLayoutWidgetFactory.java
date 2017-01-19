c0b1afba23569128b2f3a208bbf68fa73d7bcac2
syau <syau>
2017-01-19 10:52:11 +0800

    SCADAGEN-662
    Change the UIWidget parameter as a config

--- f01/workspace.fas/wrapper-widgetcontroller/src/main/java/com/thalesgroup/scadagen/wrapper/widgetcontroller/client/factory/FASLayoutWidgetFactory.java
+++ f01/workspace.fas/wrapper-widgetcontroller/src/main/java/com/thalesgroup/scadagen/wrapper/widgetcontroller/client/factory/FASLayoutWidgetFactory.java
@@ -1,13 +1,17 @@
 package com.thalesgroup.scadagen.wrapper.widgetcontroller.client.factory;
 
+import java.util.Map;
+
 import com.google.gwt.event.shared.EventBus;
-import com.thalesgroup.hypervisor.mwt.core.webapp.core.common.client.ClientLogger;
 import com.thalesgroup.hypervisor.mwt.core.webapp.core.ui.client.layout.dto.AppLayoutsData;
 import com.thalesgroup.hypervisor.mwt.core.webapp.core.ui.client.layout.dto.WidgetConfig;
 import com.thalesgroup.hypervisor.mwt.core.webapp.core.ui.client.layout.factory.HypWidgetFactory;
 import com.thalesgroup.hypervisor.mwt.core.webapp.core.ui.client.layout.view.IWidgetController;
+import com.thalesgroup.scadagen.whmi.uiutil.uilogger.client.UILogger;
+import com.thalesgroup.scadagen.whmi.uiutil.uilogger.client.UILoggerFactory;
+import com.thalesgroup.scadagen.whmi.uiutil.uiutil.client.UIWidgetUtil;
 import com.thalesgroup.scadagen.wrapper.widgetcontroller.client.UIWidgetEntryPoint;
-import com.thalesgroup.scadagen.wrapper.widgetcontroller.client.factory.FASLayoutWidgetFactory_i.FASWidget;
+import com.thalesgroup.scadagen.wrapper.widgetcontroller.client.factory.FASLayoutWidgetFactory_i.FASWidgetType;
 
 /**
  * Showcase Widget Factory
@@ -15,8 +19,8 @@ import com.thalesgroup.scadagen.wrapper.widgetcontroller.client.factory.FASLayou
 public class FASLayoutWidgetFactory extends HypWidgetFactory {
 	
 	/** logger */
-    private static final ClientLogger LOGGER = ClientLogger.getClientLogger();
-    private static final String LOG_PREFIX = "[FASLayoutWidgetFactory] ";
+	private final String className = UIWidgetUtil.getClassSimpleName(FASLayoutWidgetFactory.class.getName());
+	private UILogger logger = UILoggerFactory.getInstance().getLogger(className);
 
 	/**
 	 * Factory constructor
@@ -29,49 +33,47 @@ public class FASLayoutWidgetFactory extends HypWidgetFactory {
 
 	@Override
 	public IWidgetController buildWidget(final WidgetConfig widgetConf) {
+		final String function = "buildWidget";
+		
+		logger.begin(className, function);
 
 		final String typeId = widgetConf.getTypeId();
+		
+		logger.debug(className, function, "try to build[{}] ", typeId);
+		
 		IWidgetController widget = null;
-
-		LOGGER.debug(LOG_PREFIX + "try to build: " + typeId);
 		
-	    String uiPanel	= "UILayoutEntryPoint";
-	    String uiOpts	= null;
-	    String uiElem	= null;
-	    String uiDicts	= null;	
-		
-		if (FASWidget.PTW_DPC_PANEL.toString().equals(typeId)) {
+		if ( FASWidgetType.UIWidgetEntryPoint.toString().equals(typeId) ) {
+			Map<String, String> args = widgetConf.getArgs();
 			
-			String uiView	= "UILayoutEntryPointDpcTagSummary.view.xml";
-
-	    	widget = new UIWidgetEntryPoint(uiPanel, uiView, uiOpts, uiElem, uiDicts);
-	    	
+			logger.debug(className, function, "args.size[{}]", args.size());
+			for ( String key : args.keySet() ) {
+				logger.debug(className, function, "args.get([{}])[{}]", key, args.get(key));
+			}
+			
+			String uiCtrl	= args.get("uiCtrl");
+			String uiView	= args.get("uiView");
+			
+		    String uiOpts	= args.get("uiOpts");
+		    String uiElem	= args.get("uiElem");
+		    String uiDict	= args.get("uiDict");
+		    
+		    logger.debug(className, function, "uiCtrl[{}] ", uiCtrl);
+		    logger.debug(className, function, "uiView[{}] ", uiView);
+		    logger.debug(className, function, "uiOpts[{}] ", uiOpts);
+		    logger.debug(className, function, "uiElem[{}] ", uiElem);
+		    logger.debug(className, function, "uiDict[{}] ", uiDict);
+		    
+		    widget = new UIWidgetEntryPoint(uiCtrl, uiView, uiOpts, uiElem, uiDict);
+			
 		}
-		else if (FASWidget.LOGIN_PANEL.toString().equals(typeId)) {
-
-	    	String uiView	= "UILayoutEntryPointLoginSummary.view.xml";
-	    	
-	    	widget = new UIWidgetEntryPoint(uiPanel, uiView, uiOpts, uiElem, uiDicts);
-	    	
+		
+		if ( null == widget ) {
+			logger.warn(className, function, "typeId[{}] widget IS NULL", typeId);
 		}
-		else if (FASWidget.CHANGE_PASSWORD_PANEL.toString().equals(typeId)) {
-
-	    	String uiView	= "UILayoutOPMChangePasswordSummary.view.xml";
-	    	
-	    	widget = new UIWidgetEntryPoint(uiPanel, uiView, uiOpts, uiElem, uiDicts);
-	    	
-		}
-		else if (FASWidget.SOC_PANEL.toString().equals(typeId)) {
-
-	    	String uiView	= "UILayoutSocSummary.view.xml";
-	    	
-	    	widget = new UIWidgetEntryPoint(uiPanel, uiView, uiOpts, uiElem, uiDicts);
-	    	
-		}
-		else {
-			widget = super.buildWidget(widgetConf);
-		}
-
+		
+		logger.end(className, function);
+		
 		return widget;
 	}
 

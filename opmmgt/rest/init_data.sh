68beedaeb88b68af5847fc36012a8abfb2d65373
syau <syau>
2017-03-14 18:47:12 +0800

    SCADAGEN-700
    Update the setting for the OPM testing

--- opmmgt/rest/init_data.sh
+++ opmmgt/rest/init_data.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-curl -X POST -H "Content-Type: application/json" -d '{"category":1,"abbrev":"M","name":"VIEW_ENTITY","description":"MONITOR"}' http://127.0.0.1:12080/opm/actions
-curl -X POST -H "Content-Type: application/json" -d '{"category":2,"abbrev":"C","name":"COMMAND_ENTITY","description":"CONTROL"}' http://127.0.0.1:12080/opm/actions
-curl -X POST -H "Content-Type: application/json" -d '{"category":3,"abbrev":"D","name":"VIEW_ENTITY_TYPE_ALARM","description":"DISPLAY"}' http://127.0.0.1:12080/opm/actions
-curl -X POST -H "Content-Type: application/json" -d '{"category":4,"abbrev":"A","name":"COMMAND_ENTITY_TYPE_ALARM","description":"ALARM"}' http://127.0.0.1:12080/opm/actions
+curl -X POST -H "Content-Type: application/json" -d '{"category":1,"abbrev":"M","name":"M","description":"MONITOR"}' http://127.0.0.1:12080/opm/actions
+curl -X POST -H "Content-Type: application/json" -d '{"category":2,"abbrev":"C","name":"C","description":"CONTROL"}' http://127.0.0.1:12080/opm/actions
+curl -X POST -H "Content-Type: application/json" -d '{"category":3,"abbrev":"D","name":"D","description":"DISPLAY ALARM"}' http://127.0.0.1:12080/opm/actions
+curl -X POST -H "Content-Type: application/json" -d '{"category":4,"abbrev":"A","name":"A","description":"ALARM ACK"}' http://127.0.0.1:12080/opm/actions
 
-curl -X POST -H "Content-Type: application/json" -d '{"category":1,"family":"A","name":"FCT11","description":"function 11"}' http://127.0.0.1:12080/opm/functions
-curl -X POST -H "Content-Type: application/json" -d '{"category":2,"family":"A","name":"FCT12","description":"function 12"}' http://127.0.0.1:12080/opm/functions
+curl -X POST -H "Content-Type: application/json" -d '{"category":1,"family":"A","name":"11","description":"function 11"}' http://127.0.0.1:12080/opm/functions
+curl -X POST -H "Content-Type: application/json" -d '{"category":2,"family":"A","name":"12","description":"function 12"}' http://127.0.0.1:12080/opm/functions
 
-curl -X POST -H "Content-Type: application/json" -d '{"category":1,"name":"LOC1","description":"location 1"}' http://127.0.0.1:12080/opm/locations
-curl -X POST -H "Content-Type: application/json" -d '{"category":2,"name":"LOC2","description":"location 2"}' http://127.0.0.1:12080/opm/locations
+curl -X POST -H "Content-Type: application/json" -d '{"category":1,"name":"1","description":"location 1"}' http://127.0.0.1:12080/opm/locations
+curl -X POST -H "Content-Type: application/json" -d '{"category":2,"name":"2","description":"location 2"}' http://127.0.0.1:12080/opm/locations
 
 curl -X POST -H "Content-Type: application/json" -d '{"name":"ROOT","description":"ROOT"}' http://127.0.0.1:12080/opm/profiles
 

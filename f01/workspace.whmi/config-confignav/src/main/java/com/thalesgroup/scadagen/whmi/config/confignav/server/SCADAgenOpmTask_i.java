064006830248e28f69a78f2b127e0196d8f74c28
syau <syau>
2017-04-03 16:13:23 +0800

    SCADAGEN-724
    Split into package

---
+++ f01/workspace.whmi/config-confignav/src/main/java/com/thalesgroup/scadagen/whmi/config/confignav/server/SCADAgenOpmTask_i.java
@@ -0,0 +1,22 @@
+package com.thalesgroup.scadagen.whmi.config.confignav.server;
+
+public class SCADAgenOpmTask_i {
+	
+	public final static String setSpliter = "\\|";
+	public final static String valSpliter = ":";
+	
+	public enum AttributeValue {
+		  OR("OR")
+		, AND("AND")
+		;
+		private final String text;
+		private AttributeValue(final String text) { this.text = text; }
+		public boolean equalsName(String otherName) { return ( otherName == null ) ? false : text.equals(otherName); }
+		/* (non-Javadoc)
+		 * @see java.lang.Enum#toString()
+		 */
+		@Override
+		public String toString() { return this.text; }
+
+	}
+}
--- f01/workspace.whmi/config-confignav/src/main/java/com/thalesgroup/scadagen/whmi/config/confignav/server/TaskServiceImpl.java
+++ f01/workspace.whmi/config-confignav/src/main/java/com/thalesgroup/scadagen/whmi/config/confignav/server/TaskServiceImpl.java
@@ -4,7 +4,6 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import com.thalesgroup.scadagen.whmi.config.confignav.client.TaskService;
 import com.thalesgroup.scadagen.whmi.config.confignav.shared.Task;
-import com.thalesgroup.scadagen.whmi.config.confignav.shared.Task_i.TaskAttribute;
 import com.thalesgroup.scadagen.whmi.config.confignav.shared.Tasks;
 import com.thalesgroup.scadagen.wrapper.wrapper.server.OpmMgr;
 import com.thalesgroup.scadagen.wrapper.wrapper.server.UIOpm_i;
@@ -25,8 +24,6 @@ public class TaskServiceImpl extends RemoteServiceServlet implements TaskService
 	public Tasks taskServer(String module, String strMapping, String strSetting, String profile, String location, int targetLevel, String targetHeader) {
 		
 		final String function = "taskServer";
-		
-		UIOpm_i uiOpm_i = OpmMgr.getInstance("UIOpmSCADAgen");
 		
 		Tasks tasks = new Tasks();
 		tasks.setParentLevel(targetLevel);
@@ -62,133 +59,25 @@ public class TaskServiceImpl extends RemoteServiceServlet implements TaskService
 		
 		logger.debug("{} mapping[{}] setting[{}] tag[{}]", new Object[]{function, mapping, setting, TaskServiceImpl_i.XML_TAG});
 		
+		
+		UIOpm_i uiOpm_i = OpmMgr.getInstance("UIOpmSCADAgen");
+		SCADAgenTaskOpm taskOpm = new SCADAgenTaskOpm();
+		taskOpm.setUIOpm_i(uiOpm_i);
+		
 		ArrayList<Task> tsks = configs.getTasks(mapping, setting, TaskServiceImpl_i.XML_TAG);
 		
 		for(Task tsk: tsks) {
 			
-			String opm = tsk.getParameter(TaskAttribute.opm.toString());
-			if ( null != opm && opm.length() > 0 ) {
-				String opmOperation = tsk.getParameter(TaskAttribute.opmOperation.toString());
-				String opmName = tsk.getParameter(TaskAttribute.opmName.toString());
-				
-				logger.warn("{} opm[{}] opmOperation[{}] opmName[{}]", new Object[]{function, opm, opmOperation, opmName});
-				
-				boolean opmValid = opmCheckAccess(uiOpm_i, opm, opmOperation, opmName);
-				if ( ! opmValid ) continue;
+			int opmResult = taskOpm.taskOpmIsValid(tsk);
+			if ( opmResult < 0 ) {
+				continue;
 			}
-
+			
 			tasks.add(tsk);
 		}
 		
 		return tasks;
 	}
 	
-	final String setSpliter = "\\|";
-	final String valSpliter = ":";
-	final int lengthOfOpmName = 4;
-	final int lengthOfOpmValue = 4;
-	private boolean opmCheckAccess(UIOpm_i uiOpm_i, String opm, String opmOperation, String opmName) {
-		final String function = "opmCheckAccess";
-		
-		boolean result = false;
-		if ( null != uiOpm_i ) {
 
-			if ( null != opm && opm.length() > 0 ) {
-				
-				String [] opmNames = null;
-				if ( null != opmName && opmName.length() > 0 ) {
-					opmNames = opmName.split(valSpliter);
-					if ( null != opmNames && opmNames.length != lengthOfOpmName ) {
-						logger.warn("{} opmNames.length[{}] == lengthOfOpmName[{}] IS NOT"
-								, new Object[]{function, opmNames.length, lengthOfOpmValue});
-					}
-				}
-				
-				boolean isAndOperation = false;
-				
-				String [] opmSets = opm.split(setSpliter);
-				int opmSetsLength = opmSets.length;
-				logger.debug("{} opmSetsLength[{}]", new Object[]{function, opmSetsLength});
-				
-				if ( null != opmSets && opmSetsLength > 1 ) {
-					if ( null != opmOperation ) {
-						if ( 0 == opmOperation.compareToIgnoreCase(TaskServiceImpl_i.AttributeValue.AND.toString()) ) {
-							isAndOperation = true;
-						}
-					}
-				}
-				
-				logger.debug("{} opmOperation[{}] isAndOperation[{}]", new Object[]{function, opmOperation, isAndOperation});
-				
-				for ( int i = 0 ; i < opmSetsLength ; ++i ) {
-					if ( null != opmSets[i] ) {
-						logger.debug("{} i[{}] opmSet[{}]", new Object[]{function, i, opmSets[i]});
-						String [] opmValues = opmSets[i].split(valSpliter);
-						if ( null != opmValues ) {
-							if ( opmValues.length == lengthOfOpmValue ) {
-								
-								if (
-										   null != opmValues[0]
-										&& null != opmValues[1]
-										&& null != opmValues[2]
-										&& null != opmValues[3]
-									) {
-									
-									boolean bResult = false;
-									if ( null == opmNames || null == opmNames[0] || null == opmNames[1] || null == opmNames[2] || null == opmNames[3] ) {
-										
-										bResult = uiOpm_i.checkAccess(
-												  opmValues[0]
-												, opmValues[1]
-												, opmValues[2]
-												, opmValues[3]);
-										
-										logger.debug("{} opmValues(0)[{}] opmValues(1)[{}] opmValues(2)[{}] opmValues(3)[{}] => bResult[{}]"
-												, new Object[]{function, opmValues[0], opmValues[1], opmValues[2], opmValues[3], bResult});
-										
-									} else {
-										
-										bResult = uiOpm_i.checkAccess(
-												  opmNames[0], opmValues[0]
-												, opmNames[1], opmValues[1]
-												, opmNames[2], opmValues[2]
-												, opmNames[3], opmValues[3]);
-										
-										logger.debug("{} opmNames(0)[{}] opmValues(0)[{}] opmNames(1)[{}] opmValues(1)[{}] opmNames(2)[{}] opmValues(2)[{}] opmNames(3)[{}] opmValues(3)[{}] => bResult[{}]"
-												, new Object[]{function, opmNames[0], opmValues[0], opmNames[1], opmValues[1], opmNames[2], opmValues[2], opmNames[3], opmValues[3], bResult});
-
-									}
-									
-									if ( ! isAndOperation ) {
-										// OR Operation
-										if ( bResult ) {
-											result = true;
-											break;
-										}
-									} else {
-										// AND Operation
-										if ( bResult ) {
-											result = true;
-										} else {
-											result = false;
-											break;
-										}
-									}
-									
-								}
-							} else {
-								logger.warn("{} opmValues.length[{}] == lengthOfOpmName[{}] IS NOT", new Object[]{function, opmValues.length, lengthOfOpmName});
-							}
-						}
-					} else {
-						logger.debug("opmSets({}) IS NULL", i);
-					}
-				}
-			}
-		}
-		
-		logger.debug("result[{}]", result);
-		
-		return result;
-	}
 }

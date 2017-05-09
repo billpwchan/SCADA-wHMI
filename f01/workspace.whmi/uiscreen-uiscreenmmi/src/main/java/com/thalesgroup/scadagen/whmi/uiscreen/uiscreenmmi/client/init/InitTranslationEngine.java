e66d2d5d1fc8f04d3d06e48edc131717bbc00d20
Steven YAU <steven.yau@thalesgroup.com>
2017-05-09 20:08:31 +0800

    SCADAGEN-805
    Added the multiple support on translation

--- f01/workspace.whmi/uiscreen-uiscreenmmi/src/main/java/com/thalesgroup/scadagen/whmi/uiscreen/uiscreenmmi/client/init/InitTranslationEngine.java
+++ f01/workspace.whmi/uiscreen-uiscreenmmi/src/main/java/com/thalesgroup/scadagen/whmi/uiscreen/uiscreenmmi/client/init/InitTranslationEngine.java
@@ -1,5 +1,6 @@
 package com.thalesgroup.scadagen.whmi.uiscreen.uiscreenmmi.client.init;
 
+import com.thalesgroup.scadagen.whmi.config.configenv.client.ReadProp;
 import com.thalesgroup.scadagen.whmi.translation.translationmgr.client.TranslationEngine;
 import com.thalesgroup.scadagen.whmi.translation.translationmgr.client.TranslationMgr;
 import com.thalesgroup.scadagen.whmi.uiutil.uilogger.client.UILogger;
@@ -11,10 +12,48 @@ public class InitTranslationEngine {
 	
 	private final static String className = UIWidgetUtil.getClassSimpleName(InitTranslationEngine.class.getName());
 	private final static UILogger logger = UILoggerFactory.getInstance().getLogger(className);
+
+	private final static String strDot = ".";
+	private final static String strProperties = "properties";
+	private final static String strDCFolder = "UIInspectorPanel";
+	private final static String strTranslation = "Translation";
+	private final static String strTranslatePatten = "TranslatePatten";
+	private final static String strTranslateFlag = "TranslateFlag";
 	
 	public static void init() {
 		String function = "init";
 		logger.begin(className, function);
+		
+		String dcName = strDCFolder;
+		String dcFileName = strTranslation+strDot+strProperties;
+		{
+			String dcAttributePattern = strTranslation+strDot+strTranslatePatten;
+			
+			String translatePatten = ReadProp.readString(dcName, dcFileName, dcAttributePattern, null);
+			
+			logger.debug(className, function, "translatePatten[{}]", translatePatten);
+			
+			if ( null != translatePatten ) {
+				Translation.setTranslatePatten(translatePatten);
+			} else {
+				logger.warn(className, function, "translatePatten[{}] in dcAttributePattern[{}] is null, using default translatePatten[{}]"
+						, new Object[]{translatePatten, dcAttributePattern, Translation.getTranslatePatten()});
+			}
+		}
+		{
+			String dcAttributeFlag = strTranslation+strDot+strTranslateFlag;
+			
+			String translateFlag = ReadProp.readString(dcName, dcFileName, dcAttributeFlag, null);
+			
+			logger.debug(className, function, "translateFlag[{}]", translateFlag);
+			
+			if ( null != translateFlag ) {
+				Translation.setTranslateFlag(translateFlag);
+			} else {
+				logger.warn(className, function, "translateFlag[{}] in dcAttributeFlag[{}] is null, using default translateFlag[{}]"
+						, new Object[]{translateFlag, dcAttributeFlag, Translation.getTranslateFlag()});
+			}
+		}
 		
 		TranslationMgr.getInstance().setTranslationEngine(new TranslationEngine() {
 			@Override

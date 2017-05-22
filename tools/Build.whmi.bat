15df03ff83c1de4afe2e4838ccc3fd029f78c3ba
Steven YAU <steven.yau@thalesgroup.com>
2017-05-22 19:45:58 +0800

    SCADAGEN-781 Build and Package Scripts for SCADAgen

---
+++ tools/Build.whmi.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+SET CUR_PATH=%~dp0
+SET TOOLS_PATH=%CUR_PATH:~0,-1%
+
+CALL %TOOLS_PATH%\LoadPath.bat
+
+CALL %TOOLS_PATH%\Log.Backup.bat
+
+IF [%1] == [] SET v_strdt=%strdt%
+IF [%v_strdt%] == [] SET v_strdt=%1
+
+ECHO "v_strdt=%v_strdt%"
+
+REM start /B Launch.Tail.bat %LOG_FILE%
+
+CD %sp%
+
+ECHO starting to build...
+
+ECHO Build all...
+
+CALL %TOOLS_PATH%\Echo.LastCompilation.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.whmi.uiwidget.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.wrapper.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.whmi.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.fas.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.webapp-fun.bat %v_strdt%
+
+CALL %TOOLS_PATH%\Build.workspace.webapp.bat %v_strdt%
+ 
+ECHO End of build
+
+REM CALL %TOOLS_BASE%/Log.Backup.bat
+
+REM PAUSE

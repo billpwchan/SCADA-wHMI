ba37dffb7cc4162eaeef74fe1377807a550d161f
Steven YAU <steven.yau@thalesgroup.com>
2017-10-13 14:25:14 +0800

    SCADAGEN-321, Interface change

--- f01/workspace.whmi/uiinspector-uiinspector/src/main/java/com/thalesgroup/scadagen/whmi/uiinspector/uiinspector/client/panel/alarm/AckAlarm_i.java
+++ f01/workspace.whmi/uiinspector-uiinspector/src/main/java/com/thalesgroup/scadagen/whmi/uiinspector/uiinspector/client/panel/alarm/AckAlarm_i.java
@@ -2,15 +2,15 @@ package com.thalesgroup.scadagen.whmi.uiinspector.uiinspector.client.panel.alarm
 
 public interface AckAlarm_i {
 	
-	public final String STR_OLS_MGR = "OlsMgr";
-	public final String STR_OLS_MGR_KEY = "AckAlarm";
+	public final static String STR_OLS_MGR = "OlsMgr";
+	public final static String STR_OLS_MGR_KEY = "AckAlarm";
 	
-	public final String STR_ALM_SERVER = "AlmServer";
-	public final String STR_LIST_NAME = "AlarmList";
-	public final String STR_ALARM_FIELDS = "AlarmId";
+	public final static String STR_ALM_SERVER = "AlmServer";
+	public final static String STR_LIST_NAME = "AlarmList";
+	public final static String STR_ALARM_FIELDS = "AlarmId";
 	
-	public final String STR_KEYWORD = "{}";
-	public final String STR_FILTER_MSG_LIKE = "( Message LIKE \"{}\" )";
-	public final String STR_FILTER_CONCAT = " OR ";
-	public final String STR_FILTER = "( ( {} ) AND  (AcknowledgeRequired == 1) )";
+	public final static String STR_KEYWORD = "{}";
+	public final static String STR_FILTER_MSG_LIKE = "( Message LIKE \"{}\" )";
+	public final static String STR_FILTER_CONCAT = " OR ";
+	public final static String STR_FILTER = "( ( {} ) AND  (AcknowledgeRequired == 1) )";
 }

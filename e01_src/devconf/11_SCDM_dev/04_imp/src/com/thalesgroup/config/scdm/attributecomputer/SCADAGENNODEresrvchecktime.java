45f42d7ea0f4ff7e155fb98df411dd03b39d750a
Thomas YUEN <thomas.yuen@thalesgroup.com>
2018-01-18 14:30:34 +0800

    SCADAGEN-1181 Merge new SCADAgen_RTDB V.3 ScsModell, java computers and packing script.

---
+++ e01_src/devconf/11_SCDM_dev/04_imp/src/com/thalesgroup/config/scdm/attributecomputer/SCADAGENNODEresrvchecktime.java
@@ -0,0 +1,16 @@
+package com.thalesgroup.config.scdm.attributecomputer; 
+import com.thalesgroup.config.scdm.attributecomputer.common.EQ_TYPEresrvchecktime; 
+
+/******************************************************************************/
+/*  FILE  : SCADAGENNODEresrvchecktime.java                                   */
+/*----------------------------------------------------------------------------*/
+/*  COMPANY  : THALES IS                                                      */
+/*  CREATION DATE : 2017/07/10                                                */
+/*  LANGUAGE  : JAVA                                                          */
+/*																			  */
+/*............................................................................*/
+/*  Copyright © THALES Information Systems 1996-2003.                         */
+/*  All rights reserved.                                                      */
+/*  Unauthorized access, use, reproduction or distribution is prohibited.     */
+/******************************************************************************/
+public class SCADAGENNODEresrvchecktime extends EQ_TYPEresrvchecktime {} 

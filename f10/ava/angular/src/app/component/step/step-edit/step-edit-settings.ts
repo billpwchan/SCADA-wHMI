579e5a3f14b1ddf424dbbb2ada7c2b8e408776ff
Steven YAU <steven.yau@thalesgroup.com>
2018-02-22 19:15:46 +0800

    SCADAGEN-1227, Support filter on ava point filter

--- f10/ava/angular/src/app/component/step/step-edit/step-edit-settings.ts
+++ f10/ava/angular/src/app/component/step/step-edit/step-edit-settings.ts
@@ -11,6 +11,20 @@ export class StepEditSettings {
     public static readonly STR_LABEL    = 'label';
     public static readonly STR_VALUE    = 'value';
 
+    public static readonly STR_OLS_LST_SERVER = 'ols_lst_server';
+    public static readonly STR_OLS_LST_NAME = 'ols_lst_name';
+
     public static readonly STR_GEO_PREFIX = 'geo_prefix';
     public static readonly STR_FUNC_PREFIX = 'func_prefix';
+
+    public static readonly STR_OLS_LST_FILTER = 'ols_lst_filter';
+}
+
+export class StepEditConfig {
+    public geoPrefix: string;
+    public funcPrefix: string;
+    public envs: any;
+    public olsLstServer: string;
+    public olsLstName: string;
+    public olsLstFilter: string;
 }

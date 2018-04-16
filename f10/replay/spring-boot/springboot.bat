3e9777b2595d58b25e4ee3ae417f0e87d1f5cb9a
salau <salau>
2018-04-16 19:38:03 +0800

    FA-310 whmi create start script for each F10 module
    Add start and stop batch script for F10 modules

---
+++ f10/replay/spring-boot/springboot.bat
@@ -0,0 +1,39 @@
+set WEBAPP=REPLAYWEBAPP
+set JARPATTERN=replay*.jar
+
+title %WEBAPP%
+
+set curdirpath=%~dp0
+
+FOR %%F in (%curdirpath:~0,-1%) do set rootdir=%%~dpF
+FOR %%F in (%rootdir:~0,-1%) do set rootdir=%%~dpF
+set rootdir=%rootdir:~0,-1%
+
+REM softs pas au meme endroit en dev et sur machine cible
+if exist %rootdir%\softs goto env_cible
+REM en env de dev, softs est a cote de rootdir
+set SOFTS_DIR=%rootdir%\..\..\softs
+goto suite
+:env_cible
+set SOFTS_DIR=%rootdir%\softs
+
+:suite
+REM JAVA
+set JAVA_HOME=%SOFTS_DIR%\jdk1.7.0_80_x64
+
+REM PATH
+set PATH=%PATH%;%JAVA_HOME%\bin
+
+for /r %%f in (%JARPATTERN%) do (
+	set JAR=%%f
+	goto run
+)
+
+:notfounderror
+echo %WEBAPP% jar file not found
+goto exit
+
+:run
+java -jar %JAR%
+
+:exit
---
+++ f10/tms/spring-boot/stop.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd /D %~dp0
+set WEBAPP=TMSWEBAPP
+echo Stopping %WEBAPP%
+
+IF EXIST %WEBAPP%.pid goto killtask
+goto exit
+
+:killtask
+set /p pid=<%WEBAPP%.pid
+echo "taskkill /PID %pid%"
+taskkill /F /T /PID %pid%
+
+del /Q %WEBAPP%.pid
+
+:exit

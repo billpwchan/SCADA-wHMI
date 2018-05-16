d64dd91fab384b94ba6fec2f8fb2ebcba86fdc24
Steven YAU <steven.yau@thalesgroup.com>
2018-05-16 15:45:35 +0800

    SCADAGEN-1372, Checking with UIWidgetEntryPoint Parameter

--- f01/workspace.whmi/wrapper-widgetcontroller/src/main/java/com/thalesgroup/scadagen/wrapper/widgetcontroller/client/UIWidgetEntryPointInstant.java
+++ f01/workspace.whmi/wrapper-widgetcontroller/src/main/java/com/thalesgroup/scadagen/wrapper/widgetcontroller/client/UIWidgetEntryPointInstant.java
@@ -71,11 +71,11 @@ public class UIWidgetEntryPointInstant extends ResizeComposite implements IWidge
 		final String f = "realize";
 		logger_.begin(className_, f);
 		
-		this.uiCtrl = params.get(FASWidgetArgs.uiCtrl.toString()).toString();
-		this.uiView = params.get(FASWidgetArgs.uiView.toString()).toString();
-		this.uiOpts = params.get(FASWidgetArgs.uiOpts.toString()).toString();
-		this.uiElem = params.get(FASWidgetArgs.uiElem.toString()).toString();
-		this.uiDict = params.get(FASWidgetArgs.uiDict.toString()).toString();
+		if(null!=params.get(FASWidgetArgs.uiCtrl.toString())) this.uiCtrl = params.get(FASWidgetArgs.uiCtrl.toString()).toString();
+		if(null!=params.get(FASWidgetArgs.uiView.toString())) this.uiView = params.get(FASWidgetArgs.uiView.toString()).toString();
+		if(null!=params.get(FASWidgetArgs.uiOpts.toString())) this.uiOpts = params.get(FASWidgetArgs.uiOpts.toString()).toString();
+		if(null!=params.get(FASWidgetArgs.uiElem.toString())) this.uiElem = params.get(FASWidgetArgs.uiElem.toString()).toString();
+		if(null!=params.get(FASWidgetArgs.uiDict.toString())) this.uiDict = params.get(FASWidgetArgs.uiDict.toString()).toString();
 
 		logger_.debug(className_, f, " uiCtrl[{}] uiView[{}] uiOpts[{}] uiOpts[{}] uiDict[{}]"
 				, new Object[]{uiCtrl, uiView, uiOpts, uiElem, uiDict});

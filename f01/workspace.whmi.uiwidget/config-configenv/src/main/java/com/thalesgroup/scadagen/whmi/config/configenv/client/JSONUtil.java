6a10bbe9d665d5b999626eac1c5812aae7c2b4c4
Steven YAU <steven.yau@thalesgroup.com>
2018-06-20 15:49:24 +0800

    FA-352, Try Catch block on ReadJson API

--- f01/workspace.whmi.uiwidget/config-configenv/src/main/java/com/thalesgroup/scadagen/whmi/config/configenv/client/JSONUtil.java
+++ f01/workspace.whmi.uiwidget/config-configenv/src/main/java/com/thalesgroup/scadagen/whmi/config/configenv/client/JSONUtil.java
@@ -1,10 +1,39 @@
 package com.thalesgroup.scadagen.whmi.config.configenv.client;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
 
 public class JSONUtil {
+	
+	public static String convertJsonObject2String(JSONObject jsonObject) {
+		String ret = null;
+		if(null!=jsonObject) ret = jsonObject.toString();
+		return ret;
+	}
+	
+	public static JSONObject convertMapToJSONObject(Map<String, Object> map) {
+		JSONObject jsonObject = new JSONObject();
+		if(null!=map) {
+			for(Entry<String, Object> keyValue: map.entrySet()) {
+				String k = keyValue.getKey();
+				Object v = keyValue.getValue();
+				if(null==k||null==v) {} 
+				else if (0 == k.length()) {}
+//				else if (v instanceof Number)  { jsonObject.put(k, new JSONNumber(((Number)v).doubleValue())); }
+				else if (v instanceof Short)   { jsonObject.put(k, new JSONNumber((Short)v)); }
+				else if (v instanceof Integer) { jsonObject.put(k, new JSONNumber((Integer)v)); }
+				else if (v instanceof Double)  { jsonObject.put(k, new JSONNumber((Double)v)); }
+				else if (v instanceof Long)    { jsonObject.put(k, new JSONNumber((Long)v)); }
+				else if (v instanceof String)  { jsonObject.put(k, new JSONString((String)v)); }
+			}
+		}
+		return jsonObject;
+	}
 	
 	public static JSONArray convertStringsToJSONArray(String string[]) {
 		JSONArray jsonArray = new JSONArray();
